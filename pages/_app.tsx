c3343fa1f36c493d8840678661962af933c03857
Vinicius Zucatti <viniciuszucatti@gmail.com>
2022-01-31 21:12:45 -0300

    Add initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import '@styles/main.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Mr Saru</title>
+        <meta name="description" content="Mr Saru software house" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ templates/Home/index.tsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+  return (
+    <div>
+      <p>HOME</p>
+
+      <style jsx>{`
+        p {
+          font-size: var(--font-medium);
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default Home
